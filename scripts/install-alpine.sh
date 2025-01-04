ecec96291e5d5fad8ab12d59e79ad3741c97f91b
Matteo Cavestri <matteocavestri@MacBook-Pro-di-Matteo.local>
2025-01-04 01:12:49 +0100

    fixed night stupidity

--- scripts/install-alpine.sh
+++ scripts/install-alpine.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 apk add git
-git clone https://github.com/matteocavestri/nscdots.git /tmp
+mkdir -p /tmp/nscdots
+git clone https://github.com/matteocavestri/nscdots.git /tmp/nscdots
 
 ./tmp/nscdots/usr/sbin/setup-hardware
-./tmp/ncsdots/usr/sbin/setup-desktop-environment
-./tmp/ncsdots/usr/bin/install-nscdots
-./tmp/ncsdots/usr/bin/install-home
+./tmp/nscdots/usr/sbin/setup-desktop-environment
+./tmp/nscdots/usr/bin/install-nscdots
+./tmp/nscdots/usr/bin/install-home
