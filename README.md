594ee8e7d17265a4f7a395ebc880ab4308946ec4
Matteo Cavestri <matteocavestri@MacBook-Pro-di-Matteo.local>
2025-01-05 02:20:07 +0100

    setup zfs

--- README.md
+++ README.md
@@ -1,5 +1,95 @@
 # nscdots
 
+## Structure
+
+**BOOTMANAGER**
+refind
+
+**BOOTLOADER**
+ZFSBootMenu
+
+**FILESYSTEM**
+ZFS
+
+**INIT SYSTEM**
+OpenRC (AlpineLinux based)
+In future:  SystemD (ArchLinux based), Runit (VoidLinux based)
+
+**PACKAGE MANAGER**
+apk (AlpineLinux based)
+In future: pacman, paru (ArchLinux based), xbps (VoidLinux based)
+Added for all:
+pipx, cargo, go, npm
+
+**LOGIN MANAGER**
+elogind with elogind-polkit
+
+**AUDIO-VIDEO**
+pipewire
+pipewire-pulse pipewire-alsa pipewire-jack v4l-utils
+
+**GRAPHICS SESSIONS**
+wayland
+greetd (cage compositor)
+Sway, Labwc
+
+**SECURITY**
+iptables, ip6tables ufw
+apparmor
+
+**CONTAINER**
+podman, lxc
+distrobox
+
+**VIRTUALISATION**
+kvm, qemu
+
+**EMULATION - NOT SO MUCH EMULATION**
+waydroid, wine
+
+## Roadmap
+
+[ ] Make installation scripts
+[ ] Clean everything
+[ ] Make dotfiles
+[ ] Documenting
+[ ] Make custom tui
+[ ] Make custom gui programs
+[ ] Make iso
+[ ] Custom builds --> Xen
+
+## Workflow
+
+Minimal functional, customizable desktop environment
+Use distrobox to install programs.
+In future graphical store ...
+Multiple desktop themes ...
+System settings:
+
+- Sway
+- Labwc
+- wallpaper
+- services
+- devices
+- ...
+
+## Philosophy
+
+- Everything possible in posix shell
+- Minimal system, documentation (man)
+- Professional desktop use
+- Developer/desktop use
+
 ## Install
 
-curl -fsSL <https://raw.githubusercontent.com/matteocavestri/nscdots/main/scripts/install-alpine.sh> | sh
+- start an alpine 3.21 extended install
+- login as root
+- clone the repo: `https://github.com/matteocavestri/nscdots.git`
+- run `./scripts/alpine-zfs-prepare.sh`
+- enter in chroot
+- clone the repo: `https://github.com/matteocavestri/nscdots.git`
+- run `./scripts/alpine-zfs-install.sh`
+- reboot
+- run `./usr/sbin/setup-hardware`
+- run `./usr/sbin/setup-desktop-environment`
+- run `./usr/bin/install-nscdots`
---
+++ scripts/alpine-zfs-install.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+set -e
+
+# Function to configure essential services
+configure_services() {
+  echo "Configuring essential services..."
+  rc-update add hwdrivers sysinit
+  rc-update add networking
+  rc-update add hostname
+  apk add zfs zfs-lts zfs-scripts
+  rc-update add zfs-import sysinit
+  rc-update add zfs-mount sysinit
+}
+
+# Function to configure initramfs for ZFS
+configure_initramfs() {
+  echo "Configuring initramfs for ZFS..."
+  echo "/etc/hostid" >>/etc/mkinitfs/features.d/zfshost.files
+  echo "/etc/zfs/zroot.key" >>/etc/mkinitfs/features.d/zfshost.files
+  echo 'features="ata base keymap kms mmc scsi usb virtio nvme zfs zfshost"' >/etc/mkinitfs/mkinitfs.conf
+  mkinitfs -c /etc/mkinitfs/mkinitfs.conf "$(ls /lib/modules)"
+}
+
+# Function to configure EFI bootloader
+configure_bootloader() {
+  echo "Configuring EFI bootloader..."
+  mkfs.vfat -F32 "$BOOT_DEVICE"
+  cat <<EOF >>/etc/fstab
+$BOOT_DEVICE /boot/efi vfat defaults 0 0
+EOF
+  mkdir -p /boot/efi
+  mount /boot/efi
+  apk add curl
+  mkdir -p /boot/efi/EFI/ZBM
+  mkdir -p /boot/efi/EFI/BOOT
+  curl -o /boot/efi/EFI/ZBM/VMLINUZ.EFI -L https://get.zfsbootmenu.org/efi
+  cp /boot/efi/EFI/ZBM/VMLINUZ.EFI /boot/efi/EFI/ZBM/VMLINUZ-BACKUP.EFI
+  curl -o /boot/efi/EFI/BOOT/BOOTX64.EFI -LJ https://get.zfsbootmenu.org/efi/recovery
+  apk add refind
+  refind-install
+  rm /boot/refind_linux.conf
+
+  cat <<EOF >/boot/efi/EFI/ZBM/refind_linux.conf
+"Boot default"  "quiet loglevel=0 zbm.skip"
+"Boot to menu"  "quiet loglevel=0 zbm.show"
+EOF
+}
+
+# Main execution block
+echo "Starting in-chroot setup..."
+
+# Set root password
+echo "Please set the root password:"
+passwd
+
+# Execute functions
+configure_services
+configure_initramfs
+configure_bootloader
+
+# Final instructions
+echo "In-chroot setup completed successfully."
+echo "Before rebooting, exit the chroot and run the following commands:"
+echo "  cut -f2 -d' ' /proc/mounts | grep ^/mnt | tac | while read i; do umount -l \$i; done"
+echo "  zpool export zroot"
+echo "Then, reboot the system."
